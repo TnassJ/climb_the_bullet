bd6301df0d5a37b9aa29c84bb1b3eee37f849d1c
TnassJ <bookjo@aol.com>
2025-06-28 22:40:48 +0900

    切替修正、スコアフォント修正、インプット置き替え

--- climb_the_bullet/Assets/Script/Process/TutorialManager.cs
+++ climb_the_bullet/Assets/Script/Process/TutorialManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6e37e10cf75d456255c56d81c72d4d5f63becb8aa355bc7e27cbbee215fd746b
-size 5597
+oid sha256:9069862fe779bbdc2c0696d301f3a4a3eb8bdaa5350b7dcbd8b689211f7bbf1e
+size 5645
