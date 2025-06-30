01028435803c759d704be04fbb8b4a9beb5f7060
TnassJ <bookjo@aol.com>
2025-06-30 22:59:53 +0900

    shiftキーが効かなかったの調整

--- climb_the_bullet/Assets/Script/Inputs/GameInputs.cs
+++ climb_the_bullet/Assets/Script/Inputs/GameInputs.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8aa9a4c493af2740adc951aac1cc00f9d755e0a81ca09e99ce9f3d4cb9b37674
-size 24260
+oid sha256:c0f886dd8228f94196b714e0e3da638ee5d4e4c6ca3eae7f639bf185e8ba8e31
+size 25063
--- climb_the_bullet/Assets/Script/Player/ShootingPlayer.cs
+++ climb_the_bullet/Assets/Script/Player/ShootingPlayer.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9e67a8ed23dfd5fdb8dc1ec003b951be9436d3451fde62ee5757e84cf63db404
-size 16254
+oid sha256:db5247097d691e7870ef46634418a35d0a2a8df4bf63d0502deb8094ec4f56f7
+size 15960
