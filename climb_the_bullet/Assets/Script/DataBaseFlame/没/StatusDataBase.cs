a7430423cdace5fe4d2d65d6bb7378697be52e15
TnassJ <bookjo@aol.com>
2025-09-15 20:19:38 +0900

    追跡解除

--- climb_the_bullet/Assets/Script/DataBaseFlame/没/StatusDataBase.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-namespace App.BaseSystem.DataStores.ScriptableObjects.Status
-{
-    [CreateAssetMenu(menuName = "ScriptableObject/DataBase/Status")]
-    public class StatusDataBase : BaseDataBase<StatusData> { }
-}
